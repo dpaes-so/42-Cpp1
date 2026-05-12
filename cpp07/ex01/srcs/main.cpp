ec20842373cc470aef9d829e8b69100f27711a8a
Diogo Paes Soeiro Avila <dpaes-so@c1r1s12.42lisboa.com>
2026-05-12 15:33:49 +0100

    better main

--- cpp07/ex01/srcs/main.cpp
+++ cpp07/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 15:57:20 by dpaes-so          #+#    #+#             */
-/*   Updated: 2026/05/08 15:47:15 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2026/05/12 15:29:23 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,28 @@
 int main(void)
 {
 	const int arr[] = {1, 2, 3, 4, 5};
+	int arr2[] = {1, 2, 3, 4, 5};
 	
 	std::string sarr[] = {"hello", "world", "test"};
 
-	std::cout << "Original array: ";
-	iter(arr, 5, printInt);
+	std::cout << "const array, const print";
+	::iter(arr, 5, printInt);
 	std::cout << std::endl;
 
-	std::cout << "Original array: ";
-	iter(arr, 5, printt);
+	std::cout << "const array, same type print";
+	::iter(arr, 5, printt);
+	std::cout << std::endl;
+
+	std::cout << "no const array, same type print";
+	::iter(arr2, 5, printt);
 	std::cout << std::endl;
 	
-	iter(arr, 5, incrementInt);
-	std::cout << "After increment: ";
-	iter(arr, 5, printInt);
+	std::cout << "no const no const";
+	::iter(arr2, 5, incrementInt);
 	std::cout << std::endl;
 
-	std::cout << "String array: ";
-	iter(sarr, 3, printString);
+	std::cout << "no const string, const print";
+	::iter(sarr, 3, printString);
 	std::cout << std::endl;
-
-	std::cout << "String array: ";
-	iter(sarr, 3, printt);
-	std::cout << std::endl;
-
 	return 0;
 }
--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 15:57:20 by dpaes-so          #+#    #+#             */
-/*   Updated: 2026/05/08 17:03:59 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2026/05/12 15:33:21 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,40 @@
 
 int main()
 {
-	// try
-	// {
+	try
+	{
 		Array< int > ar(5);
 		ar[0] = 42;
-		std::cout << ar[0] << std::endl;
-		std::cout << ar.size() << std::endl;
+		std::cout << "ar first member = " << ar[0] << std::endl;
+		std::cout << "ar size = " << ar.size() << std::endl;
 		Array< int > ar2(ar);
-// 		std::cout << ar2.size() << std::endl;
-// 		Array<int> ar3 = ar;
-// 		std::cout << ar3.size() << std::endl;
-// 	}
-// 	catch(const std::exception& e)
-// 	{
-// 		std::cerr << e.what() << '\n';
-// 	}
-// 	try
-// 	{
-// 		Array< char > ar(5);
-// 		ar[0] = 42;
-// 		std::cout << ar[0] << std::endl;
-// 		std::cout << ar.size() << std::endl;
-// 		Array< char > ar2(ar);
-// 		std::cout << ar2.size() << std::endl;
-// 		Array<char> ar3 = ar;
-// 		std::cout << ar3.size() << std::endl;
-// 	}
-// 	catch(const std::exception& e)
-// 	{
-// 		std::cerr << e.what() << '\n';
-// 	}
+		std::cout << "ar2 first member = " << ar[0] << std::endl;
+		std::cout << "ar2 size = " << ar2.size() << std::endl;
+		Array<int> ar3 = ar;
+		std::cout << "ar3 first member = " << ar[0] << std::endl;
+		std::cout << "ar3 size = " << ar3.size() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Array< char  > ar(5);
+		ar[0] = 42;
+		std::cout << "ar first member = " << ar[0] << std::endl;
+		std::cout << "ar size = " << ar.size() << std::endl;
+		Array< char  > ar2(ar);
+		std::cout << "ar2 first member = " << ar[0] << std::endl;
+		std::cout << "ar2 size = " << ar2.size() << std::endl;
+		Array<char > ar3 = ar;
+		std::cout << "ar3 first member = " << ar[0] << std::endl;
+		std::cout << "ar3 size = " << ar3.size() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
 
 
