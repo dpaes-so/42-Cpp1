3f13990d9dfbde7a2498649de019b6ad25d97caf
Diogo Paes Soeiro Avila <dpaes-so@c2r8s11.42lisboa.com>
2026-05-29 18:43:47 +0100

    good work

--- cpp09/ex00/srcs/main.cpp
+++ cpp09/ex00/srcs/main.cpp
@@ -6,11 +6,17 @@
 /*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 15:57:20 by dpaes-so          #+#    #+#             */
-/*   Updated: 2026/05/28 18:05:53 by dpaes-so         ###   ########.fr       */
+/*   Updated: 2026/05/29 18:39:44 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/BitcoinExchange.hpp"
+
+std::string ltrim(const std::string& s)
+{ 
+    size_t pos = s.find_first_not_of(" \t\n\r\f\v"); 
+    return (pos == std::string::npos) ? std::string() : s.substr(pos); 
+} 
 
 bool first_parse(char *av, std::map <std::string, double> *db)
 {
@@ -22,7 +28,7 @@ bool first_parse(char *av, std::map <std::string, double> *db)
 		std::cerr << "Could not open the input file\n";
 		return(false);
 	}
-	if(data_bs.is_open() == 0)
+	if(data_bs.is_open() == 0 )
 	{
 		std::cerr << "Could not open the data.csv file\n";
 		return(false);
@@ -44,7 +50,6 @@ bool first_parse(char *av, std::map <std::string, double> *db)
 	return true;
 }
 
-
 void map_print(std::map <std::string, double> db)
 {
 	std::map<std::string, double>::iterator it2;
@@ -54,45 +59,120 @@ void map_print(std::map <std::string, double> db)
 	}
 }
 
-void start_matching(std::map <std::string, double> db,char *av)
+bool check_first_line(std::string line)
+{
+	if("date | value" != line)
+	{
+		std::cerr << "Wrong input file header\n";
+		return (false);
+	}
+	return (true);
+}
+
+bool	input_parse(std::string key, std::string btc_n,std::string line)
+{
+	if (btc_n.find_first_not_of("1234567890.") != std::string::npos
+		|| std::count(btc_n.begin(), btc_n.end(), '.') > 1)
+			return (false);
+	if (key.find_first_not_of("1234567890-") != std::string::npos
+		|| std::count(key.begin(), key.end(), '-') != 2 || key.size() != 10)
+			return (false);
+	if((btc_n.size() == 1 && btc_n.at(0) == '.' )|| btc_n.empty())
+		return(false);
+	if(!ltrim(line.substr(line.find(' '),line.find('|') - line.find(' '))).empty())
+		return(false);
+	int YY,MM,DD = 0;
+	std::string month = key.substr(key.find('-') + 1,key.find('-')).c_str();
+	YY = strtod(key.substr(0,key.find('-')).c_str(),NULL);
+	MM = strtod(month.c_str(),NULL);
+	DD = strtod(key.substr(key.find('-') + 4,key.find(' ')).c_str(),NULL);
+	if(YY > 9999 || YY < 0001|| MM > 12 || DD > 31 || DD < 1)
+		return(false);
+	switch (MM)
+	{
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			if(DD > 31)
+				return (false);
+			break;
+		case 4: case 6: case 9: case 11:
+			if(DD > 30)
+				return (false);
+			break;
+		case 2:
+			if((YY % 400 == 0) ||(YY % 4 == 0 && YY % 100 != 0))
+			{
+				if(DD > 29)
+					return(false);
+			}
+			else
+			{
+				if(DD>28)
+					return(false);
+			}
+		default:
+			return(false);
+	}
+	return (true);
+}
+
+bool start_matching(std::map <std::string, double> db,char *av)
 {
 	std::string line;
 	std::ifstream in(av); 
 	
-	// (void)db;
-	map_print(db);
+	std::getline(in,line);
+	if(!check_first_line(line))
+		return(false);
+	if(in.eof())
+		std::cerr << "Error: No dates in the input file!" << std::endl;
 	while (std::getline(in,line))
 	{
-		try
+		std::string key = line.substr(0,line.find(' '));
+		std::string btc_n = ltrim(line.substr(line.find('|') + 1 ));
+		if(input_parse(key,btc_n,line) == false)
 		{
-			std::string key = line.substr(0,line.find(' '));
-			double btc_number = strtod(line.substr(line.find('|') + 1 ).c_str(),NULL);
-			//parse
-			std::cout << key << std::endl;
-			std::cout << btc_number << std::endl;
-			std::cout << db[key] << std::endl;
-			std::cout << btc_number * db[key];
+			if(strtod(btc_n.c_str(),NULL) < 0)
+				std::cerr << "Error: Not a positive number.\n";
+			else
+				std::cerr << "Error: Bad input => " << line << std::endl;
+			continue;
 		}
-		catch(const std::exception& e)
+		double btc_number = strtod(btc_n.c_str(),NULL);
+		if(btc_number > 1000)
 		{
-			std::cerr << e.what() << '\n';
+			std::cerr << "Error: Too large a number.\n";
+			continue;
 		}
+		std::map<std::string,double>::const_iterator it = db.upper_bound(key);
+		if(it == db.begin())
+		{
+			std::cerr << "Error: Earliest date is " << (*db.begin()).first << std::endl;
+			continue;
+		}
+		std::cout << key << " => "<< btc_number << " = ";
+		it--;
+		std::cout << btc_number * (*it).second << std::endl;
 	}
-	
+	if(line.empty())
+		std::cerr << "Error: No dates in the input file!\n";
+	return (true);
 }
+
 int main(int ac, char *av[])
 {
 	if(ac == 2)
 	{		
 		std::map<std::string,double> db;
-		if(first_parse(av[1],&db) == false)
-			return 1;
-		// map_print(db);
+		if(first_parse(av[1],&db) == false)		
+			return (1);
+		if(db.empty())
+		{
+			std::cerr << "Error: Data base is empty!" << std::endl;
+			return(1);
+		}
 		start_matching(db,av[1]);
 	}
 	else
-	{
 		std::cerr << "Invalid amount of arguments(./btc <input_file>)\n";
-	}
-	return 0;
+	return (0);
 }
