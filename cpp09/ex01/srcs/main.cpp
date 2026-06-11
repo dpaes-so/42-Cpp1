ed714733f7a8aaf88587902492b67954521f278d
Avila vm <diogo.soeiroavila@gmail.com>
2026-06-11 14:59:40 +0100

    done

--- cpp09/ex01/srcs/main.cpp
+++ cpp09/ex01/srcs/main.cpp
@@ -6,13 +6,13 @@
 /*   By: finn <finn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 15:57:20 by dpaes-so          #+#    #+#             */
-/*   Updated: 2026/06/07 20:57:34 by finn             ###   ########.fr       */
+/*   Updated: 2026/06/09 18:33:10 by finn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/RPN.hpp"
 
-void calculator(std::string str,std::stack<int> &rpn)
+bool calculator(std::string str,std::stack<int> &rpn)
 {
 	int b = rpn.top(); rpn.pop();
     int a = rpn.top(); rpn.pop();
@@ -25,6 +25,11 @@ void calculator(std::string str,std::stack<int> &rpn)
 			rpn.push(a - b);
 			break;
 		case '/':
+			if(b == 0)
+			{
+				std::cerr << "Error: Division by 0\n";
+				return false;
+			}
 			rpn.push(a / b);
 			break;
 		case '*':
@@ -34,26 +39,39 @@ void calculator(std::string str,std::stack<int> &rpn)
 			std::cout << "laranja\n";
 			break;
 	}
+	return true;
 }
-void read_input(std::string input,std::stack<int> &rpn)
+bool read_input(std::string input,std::stack<int> &rpn)
 {
 	std::istringstream in(input);
 	std::string str;
-	(void)rpn;
+	
 	while(in >> str)
 	{
-		if(strchr("+*/-",str[0]))
-			calculator(str,rpn);
-		else if(isdigit(str[0]))
+		if(str.length() != 1)
 		{
-			rpn.push(str[0] - '0');
-			std::cout << rpn.size() << std::endl;
+			std::cerr << "Error: Bad character in input\n";
+			return(false);
 		}
+		if(strchr("+*/-",str[0]))
+		{
+			if(rpn.size() < 2)
+			{
+				std::cerr << "Error: Impossible to make at least one operation\n";
+				return false;
+			}
+			if(!calculator(str,rpn))
+				return(false);
+		}
+		else if(isdigit(str[0]))
+			rpn.push(str[0] - '0');
 		else
 		{
-			std::cout << "Error: Invalid argument\n";
+			std::cerr << "Error: Invalid argument\n";
+			return false;
 		}
 	}
+	return true;
 }
 
 int main(int ac, char *av[])
@@ -62,8 +80,15 @@ int main(int ac, char *av[])
 	{		
 		std::stack<int> rpn;
 		std::string input = av[1];
-		read_input(input,rpn);
-		std::cout << "Result = " << rpn.top() << std::endl;
+		if(input.empty())
+			return(std::cerr << "Error: Empty input\n",1);
+		if(read_input(input,rpn))
+		{
+			if(rpn.size() == 1)
+				std::cout << rpn.top() << std::endl;
+			else
+				std::cerr << "Error: Extra numbers in the stack\n";
+		}
 	}
 	else
 		std::cerr << "Invalid amount of arguments(./RPN <arguments>)\n";
